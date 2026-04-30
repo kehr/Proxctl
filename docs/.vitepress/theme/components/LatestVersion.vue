548572bff45459af3bd13c47bfcb279fb3e5b045
kehr <kehr.china@gmail.com>
2026-05-01 06:40:30 +0800

    fix: keep docs version metadata stable

--- docs/.vitepress/theme/components/LatestVersion.vue
+++ docs/.vitepress/theme/components/LatestVersion.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { proxctlVersion, proxctlCommit, releaseUrl } from '../../generated/version'
+import { proxctlVersion, releaseUrl } from '../../generated/version'
 </script>
 
 <template>
   <div class="proxctl-version">
     <span class="proxctl-version__label">Latest version</span>
     <a :href="releaseUrl" class="proxctl-version__value">{{ proxctlVersion }}</a>
-    <span class="proxctl-version__commit">{{ proxctlCommit }}</span>
   </div>
 </template>
